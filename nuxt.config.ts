fff9e80115ed5922fff6e34ed33fe80c977dbb80
FehimJS <fehimsakalli01@gmail.com>
2024-06-04 04:41:48 +0300

    last update html lang added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,38 +4,41 @@ export default defineNuxtConfig({
 	modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
 	app: {
 		head: {
-			title:'Film ve diziler - sinemaizle',
+			htmlAttrs: {
+				lang: "tr",
+			},
+			title: "Film ve diziler - sinemaizle",
 			link: [
 				{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-			{
-				rel: "icon",
-				type: "image/png",
-				sizes: "16x16",
-				href: "/favicon-16x16.png",
-			},
-			{
-				rel: "icon",
-				type: "image/png",
-				sizes: "32x32",
-				href: "/favicon-32x32.png",
-			},
-			{
-				rel: "apple-touch-icon",
-				sizes: "180x180",
-				href: "/apple-touch-icon.png",
-			},
-			{
-				rel: "icon",
-				type: "image/png",
-				sizes: "192x192",
-				href: "/android-chrome-192x192.png",
-			},
-			{
-				rel: "icon",
-				type: "image/png",
-				sizes: "512x512",
-				href: "/android-chrome-512x512.png",
-			},
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "16x16",
+					href: "/favicon-16x16.png",
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "32x32",
+					href: "/favicon-32x32.png",
+				},
+				{
+					rel: "apple-touch-icon",
+					sizes: "180x180",
+					href: "/apple-touch-icon.png",
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "192x192",
+					href: "/android-chrome-192x192.png",
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "512x512",
+					href: "/android-chrome-512x512.png",
+				},
 				{ rel: "manifest", href: "/site.webmanifest" },
 				{
 					rel: "stylesheet",
@@ -48,21 +51,21 @@ export default defineNuxtConfig({
 				},
 			],
 			// ************************************
-		
-		meta: [
-			{},
-			{ name: "msapplication-TileColor", content: "#2d89ef" },
-			{ name: "theme-color", content: "#ffffff" },
-		],
-	
+
+			meta: [
+				{},
+				{ name: "msapplication-TileColor", content: "#2d89ef" },
+				{ name: "theme-color", content: "#ffffff" },
+			],
+
 			// ************************************
 		},
 	},
 	runtimeConfig: {
 		ACCESS_TOKEN: process.env.ACCESS_TOKEN,
 		API_KEY: process.env.API_KEY,
-		public:{
-			baseURL:process.env.BASE_URL || 'http://localhost:3000'
-		}
+		public: {
+			baseURL: process.env.BASE_URL || "http://localhost:3000",
+		},
 	},
 });
